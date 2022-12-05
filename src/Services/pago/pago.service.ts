1401d48592ae86232a1fecec2bb6042f494c457f
Diego Zaleta <diegozaleta85.dz@gmail.com>
2022-12-05 15:54:56 -0700

    se agregó el servicio de pago

--- src/Services/pago/pago.service.ts
+++ src/Services/pago/pago.service.ts
@@ -1,4 +1,22 @@
+import { PagoModel } from './../../models/Pago.model';
+import { PagoEntity } from './../../Entity/Pago.Entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
-export class PagoService {}
+export class PagoService {
+    constructor(@InjectRepository(PagoEntity)
+                private pagoRepository: Repository<PagoEntity>){}
+
+    async create_pago(id_consumo: number, total: number, pagado:boolean){
+        const date= new Date();
+        return await this.pagoRepository.insert({
+            fecha:date,
+            idConsumo:id_consumo,
+            total:total,
+            pagado:pagado
+        }).then((res)=>console.log(res)).catch((err)=> console.log(err))
+    }
+}
