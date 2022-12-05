1cbdf0a00502e6ec8434f004c41d18b4988b7589
Kiiketchu <enriquemtz2000@gmail.com>
2022-12-05 16:08:47 -0700

    Se agregó el modulo cliente

--- src/api/cliente/cliente.module.ts
+++ src/api/cliente/cliente.module.ts
@@ -1,4 +1,13 @@
+import { ClienteEntity } from './../../Entity/Cliente.Entity';
+import { ClienteController } from './cliente.controller';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClienteServiceService } from 'src/Services/cliente/cliente.service';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([ClienteEntity])],
+    controllers: [ClienteController],
+    providers: [ ClienteServiceService],
+    exports:[TypeOrmModule]
+})
 export class ClienteModule {}
