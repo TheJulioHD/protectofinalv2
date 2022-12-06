f74d1b822206640470cccc32aaa347600b16d45c
TheJulioHD <72566333+TheJulioHD@users.noreply.github.com>
2022-12-06 07:12:29 -0700

    clientes

--- src/Services/cliente/cliente.service.ts
+++ src/Services/cliente/cliente.service.ts
@@ -1,4 +1,28 @@
+import { ClienteModel } from './../../models/Cliente.model';
+import { ClienteEntity } from './../../Entity/Cliente.Entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ClienteService {}
+export class ClienteService {
+
+    constructor(@InjectRepository(ClienteEntity)
+                private clienteRepository: Repository<ClienteEntity>){}
+
+    async create(cliente:ClienteModel){
+        return await this.clienteRepository.insert(cliente).catch((error) => console.log("error =>"+ error))
+    }
+
+    getall():Promise<ClienteEntity[]>{
+        return this.clienteRepository.find()
+    }
+
+    async getony(id:number):Promise<ClienteEntity>{
+        return  await this.clienteRepository.findOne({
+            where: {
+                id: id
+            }
+        })
+    }
+}
